6128d18cafd8de589774afafe7770838e543a29e
opelar <godmingliang@qq.com>
2018-04-08 19:19:37 +0800

    fix bug

--- src/utils/classof.js
+++ src/utils/classof.js
@@ -5,5 +5,6 @@
 export default function classof(o) {
   if (o === null) return "Null";
   if (o === undefined) return "Undefined";
+  if (isNaN(o)) return "NaN";
   return Object.prototype.toString.call(o).slice(8, -1);
 }
