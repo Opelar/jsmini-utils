aecc2df3ac94affab655ceaeb9aac44f53794984
opelar <23897631@qq.com>
2018-04-16 20:25:42 +0800

    arr-diff

---
+++ src/node_m/arr-diff.js
@@ -0,0 +1,45 @@
+function diff(arr) {
+	let len = arguments.length;
+	let idx = 0;
+	while (++idx < len) {
+		arr = diffArray(arr, arguments[idx]);
+	}
+	return arr;
+}
+
+function diffArray(one, two) {
+	if (!Array.isArray(two)) {
+		return one.slice();
+	}
+
+	let tlen = two.length;
+	let olen = one.length;
+	let idx = -1;
+	let arr = [];
+
+	while (++idx < olen) {
+		let ele = one[idx];
+		let hasEle = false;
+		let i = 0;
+
+		for (; i < tlen; i++) {
+			let val = two[i];
+
+			if (ele === val) {
+				hasEle = true;
+				break;
+			}
+		}
+
+		if (hasEle === false) {
+			arr.push(ele);
+		}
+	}
+	return arr;
+}
+
+// let a = ['a', 'b', 'c', 'd'];
+// let b = ['b', 'c'];
+// console.log(diff(a, b))
+
+export default diff;
